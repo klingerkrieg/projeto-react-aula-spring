7141d48bdf5e62e5f22a6c3dc58c2407b4c26977
klingerkrieg <klingerkrieg@gmail.com>
2023-05-17 15:51:57 -0300

    Update, Delete, Login

---
+++ src/components/Guard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Navigate } from 'react-router-dom';
+
+export default function Guard({element: Component, ...rest}){
+     let hasToken = false;
+     localStorage.getItem("token") ? hasToken = true : hasToken = false;
+     
+     if (hasToken){
+          return Component;
+     } else {
+          return <Navigate to={{ pathname: '/' }} />
+     }
+}
+
+
--- src/controllers/PeopleController.js
+++ src/controllers/PeopleController.js
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { WS_URL } from "../config";
 
 export function save(data){
     /**
@@ -11,7 +12,7 @@ export function save(data){
         console.log("salvar:");
         console.log(data);
 
-        axios.post("http://localhost:8080/people/",data).then(resp => {
+        axios.post( WS_URL + "/people/",data).then(resp => {
             console.log(resp);
             resolve(resp);
         }).catch(err => {
@@ -23,9 +24,43 @@ export function save(data){
 }
 
 
+
+export function update(data){
+   
+    return new Promise((resolve) => {
+        console.log("atualizar:");
+        console.log(data);
+        axios.put(data._links.person.href,data).then(resp => {
+            console.log(resp);
+            resolve(resp);
+        }).catch(err => {
+            console.log(err);
+            resolve(false);
+        });
+    });
+}
+
+
+export function remove(data){
+   
+    return new Promise((resolve) => {
+        console.log("deletar:");
+        console.log(data);
+        axios.delete(data._links.person.href).then(resp => {
+            console.log(resp);
+            resolve(resp);
+        }).catch(err => {
+            console.log(err);
+            resolve(false);
+        });
+    });
+}
+
+
+
 export function getAll(){
     return new Promise((resolve) => {
-        axios.get("http://localhost:8080/people").then(resp => {
+        axios.get(WS_URL + "/people").then(resp => {
             resolve(resp);
         }).catch(err => {
             resolve(false);
